dd4f5efb24a47d44abb6da17337d284515786664
Suracheth Chawla <dev1987@gmail.com>
2021-08-13 22:16:01 +0700

    functions with return values

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-fn main() {
-    println!("Hello, world!");
-
-    another_function(5, 6);
+fn five() -> i32 {
+    5
 }
 
-fn another_function(x: i32, y: i32) {
+fn main() {
+    let x = five();
+
     println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
 }
