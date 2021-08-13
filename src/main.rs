bd9027adc1d3476dd89251eff2b7cf36440cc8aa
Suracheth Chawla <dev1987@gmail.com>
2021-08-13 22:28:49 +0700

    control-flow

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    let x = plus_one(5);
+    let number = 3;
 
-    println!("The value of x is: {}", x);
-}
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 
-fn plus_one(x: i32) -> i32 {
-    x + 1
 }
